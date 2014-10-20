59140beb63926a98b673cdd83e0172adeb648cbd
Max Ehnert <max.ehnert@gmail.com>
2014-10-20 19:48:03 -0400

    gulp'd

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,79 +1,26 @@
 
-
 // FUNCTION OBJECT
 
-var ironYard = ["Tim","Wendy","Richard", "Chelsea", "Emory", "Victoria", "John", "Joanna","Max"];
-var age = ["30yrs", "25yrs", "24yrs", "23yrs", "22yrs", "31yrs", "29yrs", "35yrs", "27yrs"];
-var wendy=[];
-
-wendy.object = function( ironYard, age) {
-    if (ironYard == null) return {};
+var arr1 = [];
+var arr2 = [];
+max.object = function( arr1, arr2) {
+    if (arr1 == null) return {};
     var result = {};
 
-    for (var i = 0, length = ironYard.length; i < length; i++) {
-      if (age) {
+    for (var i = 0, length = arr1.length; i < length; i++) {
+      if (arr2) {
 
-        result[ironYard[i]] = age[i];
+        result[arr1[i]] = arr2[i];
       }
 
 
       else {
-        result[ironYard[i][0]] = ironYard[i][3];
+        result[arr1[i][0]] = arr1[i][3];
       }
     }
     return result;
   };
 
-  var w = wendy.object(ironYard,age);
-
-  console.log(w);
-
-
-// FUNCTION FIRST
-
-// var list = [1,2,3];
-// var n = max.first;
-//   max.first = function(list){
-//     return(list[0]);
-//
-//   };
-//
-//   console.log(list[0]);
-
-
-// FUNCTION SIZE
-
-
-var max = {};
-
-var myArray = [1,2,3,4,5];
-
-max.size = function (list) {
-  return(list.length);
-};
-
-var q = max.size(myArray);
-
-console.log(q);
-
-
-// FUNCTION FIRST
-
-
-  var ironYard = ["Tim","Wendy","Richard", "Chelsea", "Emory", "Victoria", "John", "Joanna","Max"];
-var n =1;
-var wendy=[];
-
-_.first = function(ironYard, n) {
-    if (ironYard == null) return void 0;
-    if (n == null) return ironYard[0];
-    if (n < 0) return [];
-    return Array.prototype.slice.call(ironYard, 0, n);
-  };
-
-  var firstOfArray= _.first(ironYard,n);
-
-  console.log(firstOfArray);
 
 // FUNCTION RANGE
 
@@ -96,74 +43,105 @@ var step = 2;
 
     return range;
   };
-console.log(max.range(start, stop, step));
-
-
-
 
 // Function Each
 
-var bob = {};
 
-bob.each = function(list,ears){
+max.each = function(list,n){
   var top = list.length
-  for(var index = 0;index < top;index = index +1){
+  for(var index = 0; index < top; index = index +1){
     var item = list[index]
-    ears(item)
-
+    n(item)
   }
+  return item;
 };
 
-bob.each(['videos','cheese','wine'],function(foo){
-  console.log(foo.length)
-});
+
+// FUNCTION FIRST
 
 
-//************ IGNORE BELOW **************//
+var n = 1;
+var arr = [];
+
+max.first = function(arr, n) {
+    if (arr == null) return void 0;
+    if (n == null) return arr[0];
+    if (n < 0) return [];
+    return Array.prototype.slice.call(arr, 0, n);
+  };
 
 
-
-// random_.random(min, max)
-// Returns a random integer between min and max, inclusive.
-// If you only pass one argument, it will return a number between 0 and that number.
-//
-// _.random(0, 100);
-// => 42
+// FUNCTION SIZE
 
 
+max.size = function (list) {
+  return(list.length);
+};
 
-//Setup my main object
-//
-var max  = {};
-//
-// max.greeting = function (message){
-//   aleart(message);
-// };
-//
-//
-// max.random = function (min, max){
-//   if (max == null){
-//     max = min;
-//     min = 0;
-//   }
-//   return min + Math.floor(Math.random() * (max -min + 1));
-// };
-//
-// max.each = function(arr){
-//   if( arr == null)return;
-//   console.log(arr);
-// }
-//
-// var a = [1,2,3,4,5];
-// max.map(a, function (num){return num * 3});
+// **************NOT MY WORK BELOW. JUST EXAMPLES FOR MY REFERENCE. *****************//
 
+(function() {
 
+	// Setup my Main Object
+	var root = this;
+	var max = {};
 
-// max.ans = function(obj) {
-//
-//     var ans = [];
-//     for (var className in obj)
-//       if ( max.contains(obj, className))
-//         ans.push(className);
-//     return ans;
-//   };
+	// Set `max` as global
+	root.max = max;
+
+	// Function will return a new array
+	// Take two parameters
+	max.map = function (arr, funct) {
+		if ( Array.isArray(arr) ) {
+			if (arr.length > 0) {
+				var res = [], i;
+				for (i = 0; i < arr.length; i++){
+					res[i] = funct(arr[i]);
+				}
+				return res;
+			}
+			return [];
+		} else {
+			return console.log('This is not an array');
+		}
+	};
+
+	// Get Current Time
+	// From Underscorejs.org
+	max.now = function (arr) {
+		return new Date().getTime();
+	};
+
+	// Greeting Function
+	max.greeting = function (message) {
+
+		// Check for Time of Day
+		var time = new Date(),
+				initial_greeting = '',
+				time_hours = time.getHours(),
+				final_greeting;
+
+		if (time_hours < 12) {
+			initial_greeting = 'Good Morning';
+		} else if (time_hours >= 12 && time_hours <= 17) {
+			initial_greeting = 'Good Afternoon';
+		} else if (time_hours > 17 && time_hours <= 24) {
+			initial_greeting = 'Good Evening';
+		}
+
+		final_greeting = (message != null) ? initial_greeting + ', ' + message : initial_greeting;
+
+		return final_greeting;
+	};
+
+	// Random Function
+	// From Underscorejs.org
+	max.random = function (min, max) {
+			if (max == null) {
+				max = min;
+				min = 0;
+			}
+	    return min + Math.floor(Math.random() * (max - min + 1));
+	};
+
+}());
